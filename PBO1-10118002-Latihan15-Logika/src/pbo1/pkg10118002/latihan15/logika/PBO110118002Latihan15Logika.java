723e9319ef4721dd16fe4ceb6c473627c9bd3c77
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:44:37 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan15-Logika/src/pbo1/pkg10118002/latihan15/logika/PBO110118002Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan15.logika;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    boolean a = true;
+    boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b)"+ !(a && b));
+    }
+
+}
